8b6312d4a82e064b05a2c61f5f782895ed4074fb
Brian Smith <brian@aicoaching.com>
2026-08-05 15:58:17 -0700

    shell: StudioShell canvas and content spacing sourced from design system tokens

--- components/studio/StudioShell.jsx
+++ components/studio/StudioShell.jsx
@@ -8,11 +8,11 @@ import { StudioAccountProvider } from "@/components/studio/StudioAccountProvider
 export default function StudioShell({ user, children }) {
   return (
     <StudioAccountProvider>
-      <main className="min-h-screen bg-black text-[var(--ink)]">
+      <main className="min-h-screen bg-[var(--canvas)] text-[var(--ink)]">
         <div className="flex min-h-screen">
           <StudioSidebar user={user} />
 
-          <section className="min-w-0 flex-1 px-5 pb-24 pt-20 sm:px-8 lg:px-10 lg:py-8">
+          <section className="min-w-0 flex-1 px-[var(--space-5)] pb-24 pt-[var(--space-20)] sm:px-[var(--space-8)] lg:px-[var(--space-10)] lg:py-[var(--space-8)]">
             <StudioMobileNav user={user} />
             <StudioTopBar user={user} />
             {children}
